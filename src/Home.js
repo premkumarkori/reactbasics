d29f3e89ff0a403870edac7cbff2109cc5e8595d
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-05-01 17:53:14 +0530

    Fourth Commit: Adding Click events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
 const Home = () => {
+  const handleClick = (e) =>{
+    console.log('hello, welcome to blog',e)
+  }
+  const handleClickAgain = (name,e) =>{
+    console.log('hello '+name,e.target);
+  }
   return (
     <div className="Home">
         <h2>HomePage</h2>
+        <button onClick={handleClick}>Clickme</button>
+        <button onClick={(e)=> handleClickAgain('Premkumar',e)}>Clickme Again</button>
     </div>
   )
 }
