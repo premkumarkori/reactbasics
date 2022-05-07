956c71d8af4b510e4d50e35986ca55aa65ea8ae4
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-05-07 10:34:12 +0530

    Fifth Commit: Using useState Hook

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
-  const handleClick = (e) =>{
-    console.log('hello, welcome to blog',e)
-  }
-  const handleClickAgain = (name,e) =>{
-    console.log('hello '+name,e.target);
+  //let name ='Premkumar';
+  const [name,setName] = useState('Premkumar');
+  const [age,setAge] = useState(26);
+  const handleClick = () =>{
+    setName('Ram');
+    setAge(27);
   }
   return (
     <div className="Home">
         <h2>HomePage</h2>
+        <p>{name} is {age} years old</p>
         <button onClick={handleClick}>Clickme</button>
-        <button onClick={(e)=> handleClickAgain('Premkumar',e)}>Clickme Again</button>
     </div>
   )
 }
