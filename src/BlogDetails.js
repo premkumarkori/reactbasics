937d06e4299e0f54dab410234d333b48576ad58c
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-05-19 16:25:43 -0500

    Final Commit: Delete Request, 404 Pages

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,18 @@
 import { useParams } from "react-router"
+import { useNavigate } from 'react-router-dom';
 import useFetch from "./useFetch"
 
 const BlogDetails = () => {
     const {id} = useParams()
     const {data:blog, error, isLoading} =useFetch('http://localhost:8000/blogs/'+id);
-     
+    const navigate =useNavigate();
+    const handleDelete =()=>{
+        fetch('http://localhost:8000/blogs/'+blog.id,{
+           method: 'DELETE' 
+        }).then(()=>{
+            navigate('/');
+        })
+    }
     return (
    <div className="blog-details">
       {isLoading && <div>Loading...</div>}
@@ -14,6 +22,7 @@ const BlogDetails = () => {
               <h2>{ blog.title }</h2>
               <p>Written by {blog.author}</p>
               <div>{blog.body}</div>
+              <button onClick={handleDelete}>Delete</button>
           </article>
       )}
    </div>
